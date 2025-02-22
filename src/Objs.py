8346af1a33f53e5b81a40f3f11a8c55bb94e2e64
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-02-22 16:50:02 +0900

    하....구리더라도 ㅅㄲ수준보다 나아

---
+++ src/Objs.py
@@ -0,0 +1,22 @@
+#나머지 부분은 다 씨* 장식이기 때문에 요건 윈도용으로 씨* 존*대충 가능하다는거 보이는거다.
+
+def __invert__(self):
+    return self.format('mersen') - self
+
+def __and__(self, x):
+    return sum([(self[i]*x[i])<<i for i in range(self.format('bit', x))])
+
+def __or__(self, x):
+    return sum([(self[i]+x[i]-(self[i]*x[i]))<<i for i in range(self.format('bit', x))])
+
+def __xor__(self, x):
+    return sum([(self[i]+x[i]-(2*(self[i]*x[i])))<<i for i in range(self.format('bit', x))])
+
+def __lshift__(self, x):
+    return ...
+
+def __rshift__(self, x):
+    return self // (2 ** x)
+
+def __getitmem__(self, x): #기분나빠서 lsb유사 시스템 가정 ㅋㅋㅋ
+    return (self<<x)>>self.format('bit') #슬라이스 미룸
